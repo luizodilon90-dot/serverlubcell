8e27f409bf3532f155e52ff4aceb8a69a38fec15
luizodilon90-dot <luizodilon90@gmail.com>
2025-11-28 04:06:41 -0300

    gsm.js

--- netlify/functions/gsm.js
+++ netlify/functions/gsm.js
@@ -3,21 +3,26 @@ export async function handler(event, context) {
     const API_KEY = process.env.GSM_API_KEY;
     const USERNAME = process.env.GSM_USERNAME;
 
+    // 🔥 Coloquei aqui os logs:
+    console.log("API_KEY:", API_KEY);
+    console.log("USERNAME:", USERNAME);
+
     const response = await fetch("https://gsmmanager.com/public/api/services", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "api-key": API_KEY,
-        "user-name": USERNAME
+        "user-name": USERNAME,
+        "Origin": "https://serverlubcell.netlify.app"
       },
       body: "{}"
     });
 
-    const json = await response.json();
+    const text = await response.text();
 
     return {
       statusCode: 200,
-      body: JSON.stringify(json)
+      body: text
     };
 
   } catch (err) {
